f9fd511e1f8b1a46976f3db55b3089672e54ca1b
Vijesh Nambiar <kudos4mvijesh@gmail.com>
2018-12-03 01:29:34 -0500

    Day 3 partial initial commit

--- src/main/java/com/vn/advent/solution/Day3.java
+++ src/main/java/com/vn/advent/solution/Day3.java
@@ -1,13 +1,54 @@
 package com.vn.advent.solution;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Stream;
 
 public class Day3 implements Solution {
 
+	public static void main(String[] args) {
+		Solution solution = new Day3();
+		solution.run();
+	}
+
 	@Override
 	public void partOne(Stream<String> lines) {
-		// TODO Auto-generated method stub
+		lines.map(this::extractParams).forEach(System.out::println);
+	}
 
+	private Claim extractParams(String str) {
+		Pattern p = Pattern.compile("\\d+");
+		Matcher m = p.matcher(str);
+		List<String> groups = new ArrayList<>();
+		while (m.find()) {
+			groups.add(m.group());
+		}
+		int[] params = groups.stream().mapToInt(Integer::parseInt).toArray();
+		Claim claim = new Claim(params[0], params[1], params[2], params[3],
+				params[4]);
+		return claim;
+	}
+
+	private static class Claim {
+		final int id;
+		final int x;
+		final int y;
+		final int width;
+		final int height;
+		private Claim(int id, int x, int y, int width, int height) {
+			this.id = id;
+			this.x = x;
+			this.y = y;
+			this.width = width;
+			this.height = height;
+		}
+		@Override
+		public String toString() {
+			return "Claim [id=" + id + ", x=" + x + ", y=" + y + ", width="
+					+ width + ", height=" + height + "]";
+		}
 	}
 
 	@Override
@@ -18,7 +59,7 @@ public class Day3 implements Solution {
 
 	@Override
 	public String getInputFileName() {
-		return "input_1";
+		return "input_3";
 	}
 
 }
