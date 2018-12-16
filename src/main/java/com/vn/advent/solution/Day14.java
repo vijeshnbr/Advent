d0f5bad1266eb51275c92b24c29a41fadcad92d8
Vijesh Nambiar <kudos4mvijesh@gmail.com>
2018-12-15 22:11:34 -0500

    Day 14 solution

--- src/main/java/com/vn/advent/solution/Day14.java
+++ src/main/java/com/vn/advent/solution/Day14.java
@@ -1,107 +1,179 @@
 package com.vn.advent.solution;
 
-import java.util.ArrayList;
-import java.util.Deque;
-import java.util.IntSummaryStatistics;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Objects;
 import java.util.logging.Level;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Day14 implements Solution {
 
-	private static final Pattern FIND_PATTERNS_PATTERN = Pattern.compile("([#|\\.]{5})\\s\\=\\>\\s#");
-
-	private static final String INITIAL_STATE = "#.#####.#.#.####.####.#.#...#.......##..##.#.#.#.###..#.....#.####..#.#######.#....####.#....##....#";
-	// private static final String INITIAL_STATE = "#..#.#..##......###...###";
-	private static final long TOTAL_GEN = 50000000000L;
+	private static final int INPUT = 170641;
 
 	public static void main(String[] args) {
 		LOGGER.setLevel(Level.OFF);
-		Solution solution = new Day12();
+		Solution solution = new Day14();
 		solution.run();
 	}
 
 	@Override
 	public void partOne(Stream<String> lines) {
-		List<Pattern> plantYieldingPatterns = lines.map(str -> {
-			String find = null;
-			Matcher m = FIND_PATTERNS_PATTERN.matcher(str);
-			if (m.find()) {
-				find = m.group(1);
-			}
-			return find;
-		})
-			.filter(Objects::nonNull)
-			.map(str -> str.replaceAll("\\.", "\\\\."))
-			// .peek(System.out::println)
-			.map(Pattern::compile)
-			.collect(Collectors.toList());
 
-		Deque<String> stackOfGenerations = new LinkedList<>();
-		String prepCurrentGenPlantStateForAnalysis = prepPlantStateForAnalysis(INITIAL_STATE);
-		// System.out.println();
-		// System.out.println(prepCurrentGenPlantStateForAnalysis);
-		stackOfGenerations.push(prepCurrentGenPlantStateForAnalysis);
-		long totalOfIndices = 0;
-		int head = 0;
+		Circle<Integer> recipeOneScore = new Circle<>(3);
+		Circle<Integer> recipeTwoScore = recipeOneScore.add(7);
+		// below 3 are running heads and need to be maintained
+		Circle<Integer> last = recipeTwoScore;
+		Circle<Integer> elfOne = recipeOneScore;
+		Circle<Integer> elfTwo = recipeTwoScore;
+		int recipeCounter = 2;
 
-		List<Integer> indicesYieldingPlantsInNextGeneration = new ArrayList<>();
-
-		for (long i = 1; i <= TOTAL_GEN; i++) {
-			String currentGen = stackOfGenerations.peek();
-			// System.out.println(i - 1 + ": " + currentGen);
-
-			int tmpHead = head;
-			plantYieldingPatterns.forEach(pattern -> {
-				// System.out.println(pattern);
-				Matcher m = pattern.matcher(currentGen);
-				while (m.find()) {
-					int start = m.start();
-					int indexOfPlant = start + 2;
-					// System.out.println("Found match " + m.group() + " at "
-					// + (indexOfPlant - 4));
-					indicesYieldingPlantsInNextGeneration.add(indexOfPlant - (4 - tmpHead));
-					m.region(start + 1, currentGen.length() - 1);
+		int tenPlusInput = INPUT + 10;
+		while (true) {
+			// calculate new recipe score and add to end of scores
+			Integer total = elfOne.current + elfTwo.current;
+			if (total / 10 == 1) {
+				last = last.add(1);
+				recipeCounter++;
+				if (recipeCounter > INPUT && recipeCounter <= tenPlusInput) {
+					System.out.print(last.current);
 				}
-			});
-			StringBuilder nextGen = new StringBuilder(currentGen.replaceAll("#", "."));
-			indicesYieldingPlantsInNextGeneration.forEach(index -> nextGen.setCharAt(index + (4 - tmpHead), '#'));
-			String prepNextGenPlantStateForAnalysis = prepPlantStateForAnalysis(nextGen.toString());
-			// System.out.println(prepNextGenPlantStateForAnalysis);
-			stackOfGenerations.push(prepNextGenPlantStateForAnalysis);
-			IntSummaryStatistics summaryStatistics = indicesYieldingPlantsInNextGeneration.stream()
-				.mapToInt(Integer::valueOf)
-				.summaryStatistics();
-			head = summaryStatistics.getMin();
-			if (i == TOTAL_GEN)
-				totalOfIndices = summaryStatistics.getSum();
-			indicesYieldingPlantsInNextGeneration.sort(Integer::compare);
-			// System.out.println(indicesYieldingPlantsInNextGeneration);
-			indicesYieldingPlantsInNextGeneration.clear();
+				if (recipeCounter == tenPlusInput)
+					break;
+			}
+			last = last.add(total % 10);
+			recipeCounter++;
+			if (recipeCounter > INPUT && recipeCounter <= tenPlusInput) {
+				System.out.print(last.current);
+			}
+			if (recipeCounter == tenPlusInput)
+				break;
+
+			// pick new recipes
+			elfOne = elfOne.getElementAfterNSteps(1 + elfOne.current);
+			elfTwo = elfTwo.getElementAfterNSteps(1 + elfTwo.current);
 		}
-
-		System.out.println(totalOfIndices);
-	}
-
-	private String prepPlantStateForAnalysis(String s) {
-		int firstIndexOfPlant = s.indexOf('#');
-		int lastIndexOfPlant = s.lastIndexOf('#');
-		return "...." + s.substring(firstIndexOfPlant, lastIndexOfPlant + 1) + "....";
 	}
 
 	@Override
 	public void partTwo(Stream<String> lines) {
+		Circle<Integer> recipeOneScore = new Circle<>(3);
+		Circle<Integer> recipeTwoScore = recipeOneScore.add(7);
+		String initialScore = "37";
+		String input = String.valueOf(INPUT);
+		int length = input.length();
+		// below 3 are running heads and need to be maintained
+		Circle<Integer> last = recipeTwoScore;
+		Circle<Integer> elfOne = recipeOneScore;
+		Circle<Integer> elfTwo = recipeTwoScore;
+		int recipeCounter = 2;
 
+		StringBuilder lastNoOfScoresAsDigitsInInput = new StringBuilder(initialScore);
+		while (true) {
+			// calculate new recipe score and add to end of scores
+			Integer total = elfOne.current + elfTwo.current;
+			if (total / 10 == 1) {
+				last = last.add(1);
+				recipeCounter++;
+				lastNoOfScoresAsDigitsInInput.append(Character.forDigit(last.current, 10));
+				if (lastNoOfScoresAsDigitsInInput.length() > length) {
+					lastNoOfScoresAsDigitsInInput = new StringBuilder(lastNoOfScoresAsDigitsInInput.substring(1));
+				}
+				if (input.equals(lastNoOfScoresAsDigitsInInput.toString())) {
+					System.out.print(recipeCounter - length);
+					break;
+				}
+			}
+			last = last.add(total % 10);
+			recipeCounter++;
+			lastNoOfScoresAsDigitsInInput.append(Character.forDigit(last.current, 10));
+			if (lastNoOfScoresAsDigitsInInput.length() > length) {
+				lastNoOfScoresAsDigitsInInput = new StringBuilder(lastNoOfScoresAsDigitsInInput.substring(1));
+			}
+			if (input.equals(lastNoOfScoresAsDigitsInInput.toString())) {
+				System.out.print(recipeCounter - length);
+				break;
+			}
+			// pick new recipes
+			elfOne = elfOne.getElementAfterNSteps(1 + elfOne.current);
+			elfTwo = elfTwo.getElementAfterNSteps(1 + elfTwo.current);
+		}
 	}
 
-	public static int indexOf(Pattern pattern, String s) {
-		Matcher matcher = pattern.matcher(s);
-		return matcher.find() ? matcher.start() : -1;
+	/**
+	 * A circular doubly linked list collection basic implementation with only
+	 * methods I need
+	 */
+	static class Circle<T> {
+		T current;
+		Circle<T> previous;
+		Circle<T> next;
+
+		Circle(T element) {
+			this.current = element;
+			previous = this;
+			next = this;
+		}
+
+		Circle<T> add(T element) {
+			Circle<T> last;
+			if (current == null) {
+				current = element;
+				previous = this;
+				next = this;
+				last = this;
+			} else {
+				Circle<T> add = new Circle<>(element);
+				add.next = this.next;
+				add.previous = this;
+				add.next.previous = add;
+				this.next = add;
+				last = add;
+			}
+			return last;
+		}
+
+		Circle<T> getElementAfterNSteps(int n) {
+			Circle<T> current = this;
+			for (int i = 1; i <= n; i++) {
+				current = current.next;
+			}
+			return current;
+		}
+
+		/**
+		 * Use this method to debug - it will print the circle as in problem and
+		 * also highlight the input firstHead and secondHead
+		 * 
+		 * @param currentHead
+		 * @return
+		 */
+		String toString(Circle<T> firstHead, Circle<T> secondHead) {
+			Circle<T> printHead = this;
+			StringBuilder sb = new StringBuilder();
+			if (printHead == firstHead)
+				sb.append('(');
+			else if (printHead == secondHead)
+				sb.append('[');
+			sb.append(printHead.current);
+			if (printHead == firstHead)
+				sb.append(')');
+			else if (printHead == secondHead)
+				sb.append(']');
+			sb.append(" ");
+			printHead = this.next;
+			while (printHead != this) {
+				if (printHead == firstHead)
+					sb.append('(');
+				else if (printHead == secondHead)
+					sb.append('[');
+				sb.append(printHead.current);
+				if (printHead == firstHead)
+					sb.append(')');
+				else if (printHead == secondHead)
+					sb.append(']');
+				sb.append(" ");
+				printHead = printHead.next;
+			}
+			return sb.toString();
+		}
+
 	}
 
 	@Override
