6535e118befa6d802fef52349e7e9b5f94c29773
Vijesh Nambiar <kudos4mvijesh@gmail.com>
2018-12-19 10:22:33 -0500

    Day 18 solution

--- src/main/java/com/vn/advent/solution/Day18.java
+++ src/main/java/com/vn/advent/solution/Day18.java
@@ -19,16 +19,30 @@ public class Day18 implements Solution {
 
 	@Override
 	public void partOne(Stream<String> lines) {
-		List<String> allLines = lines.collect(Collectors.toList());
+		List<String> allLines = lines.collect( Collectors.toList() );
 		int y = 0;
-		for (String line : allLines) {
+		for( String line : allLines )
+		{
 			char[] acresInLine = line.toCharArray();
-			for (int x = 0; x < acresInLine.length; x++) {
+			for( int x = 0; x < acresInLine.length; x++ )
+			{
 				char c = acresInLine[x];
-				Coordinates loc = new Coordinates(x, y);
-				Acre.acres.put(loc, Acre.of(c, loc));
+				Coordinates loc = new Coordinates( x, y );
+				Acre.acres.put( loc, Acre.of( c, loc ) );
 			}
+			y++;
 		}
+
+		for( int i = 1; i <= 10; i++ )
+		{
+			Map<Coordinates, Acre> changedLandscape = Acre.acres.values().stream().parallel().map( Acre::getNewValue ).collect( Collectors.toMap( Acre::getCoordinates, Function.identity() ) );
+			Acre.acres.clear();
+			Acre.acres.putAll( changedLandscape );
+		}
+
+		long countLumberYard = Acre.acres.values().stream().map( Acre::type ).filter( Type.LUMBERYARD::equals ).count();
+		long countAcresWithTrees = Acre.acres.values().stream().map( Acre::type ).filter( Type.TREES::equals ).count();
+		System.out.print( countLumberYard * countAcresWithTrees );
 	}
 
 	@Override
@@ -36,179 +50,295 @@ public class Day18 implements Solution {
 
 	}
 
-	static enum Type {
-		GROUND, TREES, LUMBERYARD
-	}
-
-	static interface Acre {
-		public static final Map<Coordinates, Acre> acres = new HashMap<>();
+	static interface Acre
+	{
+		public static final Map<Coordinates, Acre> acres = new TreeMap<>( Comparator.comparing( Coordinates::getY )
+						.thenComparing( Coordinates::getX ) );
 
 		Type type();
 
 		Coordinates getCoordinates();
 
-		static Acre of(char c, Coordinates loc) {
-			Acre a = null;
-			if (c == '.') {
-				a = new Ground(loc);
-			} else if (c == '#') {
-				a = new LumberYard(loc);
-			} else if (c == '|') {
-				a = new Trees(loc);
+		Predicate<Acre> isGroundAndSurroundedBy3orMoreAcresWithTrees = a ->
+			{
+				boolean isGround = a.type() == Type.GROUND;
+				long count = Acre.neighbors( a ).stream().map( Acre::type ).filter( Type.TREES::equals ).count();
+				boolean isSurroundedBy3orMoreAcresWithTrees = count >= 3;
+				return isGround && isSurroundedBy3orMoreAcresWithTrees;
+			};
+		Predicate<Acre> isTreesAndSurroundedBy3orMoreLumberYards = a ->
+			{
+				boolean isTrees = a.type() == Type.TREES;
+				long count = Acre.neighbors( a ).stream().map( Acre::type ).filter( Type.LUMBERYARD::equals ).count();
+				boolean isSurroundedBy3orMoreLumberYards = count >= 3;
+				return isTrees && isSurroundedBy3orMoreLumberYards;
+			};
+
+		Predicate<Acre> isLumberYardAndNotSurroundedBy1orMoreLumberYardAnd1OrMoreAcresWithTrees = a ->
+			{
+				boolean isLumberYard = a.type() == Type.LUMBERYARD;
+				long countLumberYard = Acre.neighbors( a ).stream().map( Acre::type ).filter( Type.LUMBERYARD::equals ).count();
+				long countAcresWithTrees = Acre.neighbors( a ).stream().map( Acre::type ).filter( Type.TREES::equals ).count();
+				boolean isSurroundedBy1orMoreLumberYardAnd1OrMoreAcresWithTrees = countLumberYard >= 1 && countAcresWithTrees >= 1;
+				return isLumberYard && !isSurroundedBy1orMoreLumberYardAnd1OrMoreAcresWithTrees;
+			};
+
+		default Acre getNewValue()
+		{
+			Acre a = this;
+			Coordinates c = a.getCoordinates();
+			if( isGroundAndSurroundedBy3orMoreAcresWithTrees.test( a ) )
+			{
+				a = new Trees( c );
+			}
+			else if( isTreesAndSurroundedBy3orMoreLumberYards.test( a ) )
+			{
+				a = new LumberYard( c );
+			}
+			else if( isLumberYardAndNotSurroundedBy1orMoreLumberYardAnd1OrMoreAcresWithTrees.test( a ) )
+			{
+				a = new Ground( c );
 			}
 			return a;
 		}
 
-		static List<Acre> neighbors(Coordinates c) {
-			return c.getNeighbors()
-				.stream()
-				.map(acres::get)
-				.filter(Objects::nonNull)
-				.collect(Collectors.toList());
+		static Acre of(char c, Coordinates loc)
+		{
+			Acre a = null;
+			if( c == '.' )
+			{
+				a = new Ground( loc );
+			}
+			else if( c == '#' )
+			{
+				a = new LumberYard( loc );
+			}
+			else if( c == '|' )
+			{
+				a = new Trees( loc );
+			}
+			return a;
+		}
+
+		static List<Acre> neighbors(Acre a)
+		{
+			return a.getCoordinates().getNeighbors()
+							.stream()
+							.map( acres::get )
+							.filter( Objects::nonNull )
+							.collect( Collectors.toList() );
 		}
 	}
 
-	static class Ground implements Acre {
+	static class Ground implements Acre
+	{
 
 		Coordinates c;
 
-		Ground(Coordinates c) {
+		Ground(Coordinates c)
+		{
 			this.c = c;
 		}
 
 		@Override
-		public Type type() {
+		public Type type()
+		{
 			return Type.GROUND;
 		}
 
 		@Override
-		public Coordinates getCoordinates() {
+		public Coordinates getCoordinates()
+		{
 			return this.c;
+		}
+
+		@Override
+		public String toString()
+		{
+			return ".";
 		}
 
 	}
 
-	static class Trees implements Acre {
+	static class Trees implements Acre
+	{
 		Coordinates c;
 
-		Trees(Coordinates c) {
+		Trees(Coordinates c)
+		{
 			this.c = c;
 		}
 
 		@Override
-		public Type type() {
+		public Type type()
+		{
 			return Type.TREES;
 		}
 
 		@Override
-		public Coordinates getCoordinates() {
+		public Coordinates getCoordinates()
+		{
 			return this.c;
+		}
+
+		@Override
+		public String toString()
+		{
+			return "|";
 		}
 
 	}
 
-	static class LumberYard implements Acre {
+	static class LumberYard implements Acre
+	{
 		Coordinates c;
 
-		LumberYard(Coordinates c) {
+		LumberYard(Coordinates c)
+		{
 			this.c = c;
 		}
 
 		@Override
-		public Type type() {
+		public Type type()
+		{
 			return Type.LUMBERYARD;
 		}
 
 		@Override
-		public Coordinates getCoordinates() {
+		public Coordinates getCoordinates()
+		{
 			return this.c;
 		}
 
+		@Override
+		public String toString()
+		{
+			return "#";
+		}
 	}
 
 	@Override
-	public String getInputFileName() {
-		return "test_input";
+	public String getInputFileName()
+	{
+		return "input";
 	}
 
-	static class Coordinates {
+	static enum Type
+	{
+		GROUND, TREES, LUMBERYARD
+	}
+
+	static class Coordinates
+	{
 		final int x, y;
 
-		Coordinates(int x, int y) {
+		Coordinates(int x, int y)
+		{
 			this.x = x;
 			this.y = y;
 		}
 
-		public Coordinates left() {
-			return new Coordinates(x - 1, y);
+		public Coordinates left()
+		{
+			return new Coordinates( x - 1, y );
 		}
 
-		public Coordinates right() {
-			return new Coordinates(x + 1, y);
+		public Coordinates right()
+		{
+			return new Coordinates( x + 1, y );
 		}
 
-		public Coordinates up() {
-			return new Coordinates(x, y - 1);
+		public Coordinates up()
+		{
+			return new Coordinates( x, y - 1 );
 		}
 
-		public Coordinates down() {
-			return new Coordinates(x, y + 1);
+		public Coordinates down()
+		{
+			return new Coordinates( x, y + 1 );
 		}
 
-		public Coordinates upleft() {
-			return new Coordinates(x - 1, y - 1);
+		public Coordinates upleft()
+		{
+			return new Coordinates( x - 1, y - 1 );
 		}
 
-		public Coordinates upright() {
-			return new Coordinates(x + 1, y + 1);
+		public Coordinates upright()
+		{
+			return new Coordinates( x + 1, y - 1 );
 		}
 
-		public Coordinates downleft() {
-			return new Coordinates(x - 1, y + 1);
+		public Coordinates downleft()
+		{
+			return new Coordinates( x - 1, y + 1 );
 		}
 
-		public Coordinates downright() {
-			return new Coordinates(x + 1, y + 1);
+		public Coordinates downright()
+		{
+			return new Coordinates( x + 1, y + 1 );
 		}
 
-		public List<Coordinates> getNeighbors() {
+		public List<Coordinates> getNeighbors()
+		{
 			List<Coordinates> list = new ArrayList<>();
-			list.add(up());
-			list.add(down());
-			list.add(left());
-			list.add(right());
-			list.add(upleft());
-			list.add(downleft());
-			list.add(upright());
-			list.add(downright());
+			list.add( up() );
+			list.add( down() );
+			list.add( left() );
+			list.add( right() );
+			list.add( upleft() );
+			list.add( downleft() );
+			list.add( upright() );
+			list.add( downright() );
 			return list;
 		}
 
-		@Override
-		public int hashCode() {
-			return Objects.hash(x, y);
+		int getX()
+		{
+			return x;
+		}
+
+		int getY()
+		{
+			return y;
 		}
 
 		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
+		public int hashCode()
+		{
+			return Objects.hash( x, y );
+		}
+
+		@Override
+		public boolean equals(Object obj)
+		{
+			if( this == obj )
+			{
 				return true;
-			if (obj == null)
+			}
+			if( obj == null )
+			{
 				return false;
-			if (getClass() != obj.getClass())
+			}
+			if( getClass() != obj.getClass() )
+			{
 				return false;
-			Coordinates other = (Coordinates) obj;
-			if (x != other.x)
+			}
+			Coordinates other = (Coordinates)obj;
+			if( x != other.x )
+			{
 				return false;
-			if (y != other.y)
+			}
+			if( y != other.y )
+			{
 				return false;
+			}
 			return true;
 		}
 
 		@Override
-		public String toString() {
+		public String toString()
+		{
 			return x + "," + y;
 		}
 	}
-
 }
