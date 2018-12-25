611444d2df3ad3ac49c105393b5ad895808205c7
Vijesh Nambiar <kudos4mvijesh@gmail.com>
2018-12-25 18:52:04 -0500

    Day 25 cleanup

--- src/main/java/com/vn/advent/solution/Day25.java
+++ src/main/java/com/vn/advent/solution/Day25.java
@@ -1,9 +1,9 @@
 package com.vn.advent.solution;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 import java.util.Set;
 import java.util.logging.Level;
 import java.util.stream.Collectors;
@@ -21,50 +21,93 @@ public class Day25 implements Solution {
 	public void partOne(Stream<String> lines) {
 		List<Point> points = lines.map(Point::new)
 			.collect(Collectors.toList());
-		Set<Set<Point>> constellations = new HashSet<>();
-		System.out.println();
+		Set<Constellation> constellations = new HashSet<>();
 		for (Point p : points) {
-			boolean pAddedToAConstellation = false;
-			Set<Set<Point>> setOfConstellationsToBeMerged = new HashSet<>();
-			for (Set<Point> constellation : constellations) {
-				boolean added = addData(constellation, p);
-				if (added) {
-					pAddedToAConstellation = true;
-					setOfConstellationsToBeMerged.add(constellation);
-				}
-			}
-			if (setOfConstellationsToBeMerged.size() > 1) {
-				Set<Point> mergedConstellation = new HashSet<>();
-				setOfConstellationsToBeMerged.stream()
-					.forEach(mergedConstellation::addAll);
-				Set<Set<Point>> tmpConst = new HashSet<>(constellations);
-				constellations = tmpConst;
-				setOfConstellationsToBeMerged.stream()
-					.forEach(constellations::remove);
-				constellations.add(mergedConstellation);
-			}
-			if (!pAddedToAConstellation) {
-				// Create a new constellation with the point and add it to set
-				// of constellations
-				constellations.add(new HashSet<Point>(Arrays.asList(p)));
-			}
+			Set<Constellation> afterAdd = constellations.stream()
+				.map(c -> c.add(p))
+				.collect(Collectors.toSet());
+			Optional<Constellation> merged = afterAdd.stream()
+				.filter(c -> c.contains(p))
+				.reduce(Constellation::merge);
+			Set<Constellation> rest = afterAdd.stream()
+				.filter(c -> !c.contains(p))
+				.collect(Collectors.toSet());
+			rest.add(merged.orElse(Constellation.of(p)));
+			constellations = rest;
 		}
 		System.out.print(constellations.size());
 	}
 
-	private boolean addData(Set<Point> constellation, Point point) {
-		boolean added = false;
-		for (Point p : constellation) {
-			if (p.distanceTo(point) <= 3) {
-				p.neighbors.add(point);
-				point.neighbors.add(p);
-				added = true;
+	@Override
+	public void partTwo(Stream<String> lines) {
+		// TODO Auto-generated method stub
+
+	}
+
+	static class Constellation {
+		final Set<Point> points;
+
+		Constellation(Set<Point> points) {
+			this.points = points;
+		}
+
+		static Constellation of(Point p) {
+			Set<Point> setOfOnePoint = new HashSet<>();
+			setOfOnePoint.add(p);
+			return new Constellation(setOfOnePoint);
+		}
+
+		Constellation add(Point point) {
+			Set<Point> morePoints = new HashSet<>(this.points);
+			boolean added = false;
+			for (Point p : morePoints) {
+				if (p.distanceTo(point) <= 3) {
+					p.neighbors.add(point);
+					point.neighbors.add(p);
+					added = true;
+				}
 			}
+			if (added)
+				morePoints.add(point);
+			return new Constellation(morePoints);
 		}
-		if (added) {
-			constellation.add(point);
+
+		boolean contains(Point p) {
+			return this.points.contains(p);
 		}
-		return added;
+
+		Constellation merge(Constellation other) {
+			Set<Point> allPoints = new HashSet<>(this.points);
+			allPoints.addAll(other.points);
+			return new Constellation(allPoints);
+		}
+
+		@Override
+		public int hashCode() {
+			return Objects.hash(points);
+		}
+
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Constellation other = (Constellation) obj;
+			if (points == null) {
+				if (other.points != null)
+					return false;
+			} else if (!points.equals(other.points))
+				return false;
+			return true;
+		}
+
+		@Override
+		public String toString() {
+			return points.toString();
+		}
 	}
 
 	static class Point {
@@ -113,12 +156,6 @@ public class Day25 implements Solution {
 		public String toString() {
 			return "<" + x + "," + y + "," + z + "," + t + ">";
 		}
-	}
-
-	@Override
-	public void partTwo(Stream<String> lines) {
-		// TODO Auto-generated method stub
-
 	}
 
 	@Override
