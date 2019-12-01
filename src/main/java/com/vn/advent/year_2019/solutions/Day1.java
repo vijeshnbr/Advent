52aefcb7481889e3e14badf59c71edcdf448aa71
Vijesh Nambiar <kudos4mvijesh@gmail.com>
2019-12-01 18:34:15 -0500

    Day 1 solution

--- src/main/java/com/vn/advent/year_2019/solutions/Day1.java
+++ src/main/java/com/vn/advent/year_2019/solutions/Day1.java
@@ -1,8 +1,8 @@
 package com.vn.advent.year_2019.solutions;
 
-import com.vn.advent.Solution;
-
 import java.util.stream.Stream;
+
+import com.vn.advent.Solution;
 
 public class Day1 implements Solution {
 
@@ -12,13 +12,32 @@ public class Day1 implements Solution {
 	}
 
 	public String partOne(Stream<String> lines) {
-		return "Solving part 1";
+
+		long fuel = lines.mapToInt(Integer::parseInt)
+				.map(mass -> mass / 3 - 2)
+				.sum();
+		return String.valueOf(fuel);
 	}
 
 	public String partTwo(Stream<String> lines) {
-		return "Solving part 2";
+		long fuelMass = lines.mapToInt(Integer::parseInt)
+				.map(this::calculateFuelForMass)
+				.sum();
+		return String.valueOf(fuelMass);
 	}
 
+
+	private int calculateFuelForMass(int mass) {
+		return calculateFuelForMassHelper(mass, 0);
+	}
+
+	private int calculateFuelForMassHelper(int mass, int previousTotalMass) {
+		if (mass <= 0)
+			return previousTotalMass;
+		int fuelMass = mass / 3 - 2;
+		fuelMass = fuelMass > 0 ? fuelMass : 0;
+		return calculateFuelForMassHelper(fuelMass, previousTotalMass + fuelMass);
+	}
 
 	@Override
 	public String getInputFileName() {
