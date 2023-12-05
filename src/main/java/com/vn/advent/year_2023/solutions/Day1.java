8ebfb62be8c8ff514fe195d875921bb852845210
nambiarv <Vijesh.Nambiar@moodys.com>
2023-12-05 11:58:06 -0500

    Day 1 - 2023 - found a generic solution, no replace, no hard code

--- src/main/java/com/vn/advent/year_2023/solutions/Day1.java
+++ src/main/java/com/vn/advent/year_2023/solutions/Day1.java
@@ -10,9 +10,8 @@ import java.util.stream.Stream;
 public class Day1 implements Solution {
 
     private static final Pattern PATTERN = Pattern.compile("(?<first>\\d).*(?<last>\\d)|(?<single>\\d)");
-    private static final Pattern PATTERN_PART_TWO = Pattern.compile("(?<first>(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|\\d).*(?<last>(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|\\d)|(?<single>(twone)|(oneight)|(eightwo)|(nineight)|(fiveight)|(sevenine)|(threeight)|(eighthree)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|\\d)");
+    private static final Pattern PATTERN_PART_TWO = Pattern.compile("(?<first>(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|\\d).*(?<last>(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine)|\\d)|(?<singleword>(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))|(?<singledigit>\\d)");
     private static final Map<String, String> WORD_TO_DIGIT_MAP = Map.of("one", "1", "two", "2", "three", "3", "four", "4", "five", "5", "six", "6", "seven", "7", "eight", "8", "nine", "9");
-    private static final Map<String, String> WORD_TO_NUMBER_OVERLAP = Map.of("twone", "21", "oneight", "18", "eightwo", "82", "nineight", "98", "fiveight", "58", "sevenine", "79", "threeight", "38", "eighthree", "83");
 
 
     public static void main(String[] args) {
@@ -32,9 +31,18 @@ public class Day1 implements Solution {
         return String.valueOf(lines.map(PATTERN_PART_TWO::matcher)
                 .filter(Matcher::find)
                 .map(matcher -> {
-                    final String singleWordOrDigit = matcher.group("single");
-                    if (singleWordOrDigit != null)
-                        return WORD_TO_NUMBER_OVERLAP.getOrDefault(singleWordOrDigit, WORD_TO_DIGIT_MAP.getOrDefault(singleWordOrDigit, singleWordOrDigit + "" + singleWordOrDigit));
+                    final String singleDigit = matcher.group("singledigit");
+                    if(singleDigit!=null)
+                        return singleDigit + "" + singleDigit;
+                    final String singleWord = matcher.group("singleword");
+                    if(singleWord!=null) {
+                        if(matcher.find(matcher.start()+1)) {
+                            final String overlappedWord = matcher.group("singleword");
+                            if(overlappedWord != null) {
+                                return WORD_TO_DIGIT_MAP.get(singleWord) + WORD_TO_DIGIT_MAP.get(overlappedWord);
+                            }
+                        }
+                    }
                     final String firstWordOrDigit = matcher.group("first");
                     final String lastWordOrDigit = matcher.group("last");
                     return WORD_TO_DIGIT_MAP.getOrDefault(firstWordOrDigit, firstWordOrDigit) + WORD_TO_DIGIT_MAP.getOrDefault(lastWordOrDigit, lastWordOrDigit);
